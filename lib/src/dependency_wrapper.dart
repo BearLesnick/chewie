8b9e329f9764181d717fb45774fca7610de9c5e2
vzaika <ecw3R7iTyasgraAt>
2019-09-11 13:13:59 +0300

    change update policy

--- lib/src/dependency_wrapper.dart
+++ lib/src/dependency_wrapper.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
 
 class DependenciesWrapper<T> extends InheritedWidget {
-  final Widget child;
-  final T dependence;
+  DependenciesWrapper({Widget child, this.dependence}) : super(child: child);
 
-  DependenciesWrapper({this.child, this.dependence}) : super(child: child);
+  final T dependence;
 
   static Type _typeOf<T>() => T;
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) {
     if (oldWidget is DependenciesWrapper) {
-      return oldWidget.child == this.child;
+      return oldWidget.dependence == this.dependence;
     }
     return false;
   }
 
   static DependenciesWrapper<T> of<T>(BuildContext context) {
     Type type = _typeOf<DependenciesWrapper<T>>();
-    return context.inheritFromWidgetOfExactType(type)
-    as DependenciesWrapper<T>;
+    return context.inheritFromWidgetOfExactType(type) as DependenciesWrapper<T>;
   }
 }
